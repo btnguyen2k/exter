b8155a2e7766384b7f27a1e6134af21152740d48
Thanh Nguyen <thanhn@microsoft.com>
2020-12-03 17:07:55 +1100

    update test scripts

--- be-api/src/gvabe/bo/app/dao_app_sql_test.go
+++ be-api/src/gvabe/bo/app/dao_app_sql_test.go
@@ -3,6 +3,7 @@ package app
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -14,6 +15,8 @@ import (
 	_ "github.com/godror/godror"
 	_ "github.com/jackc/pgx/v4/stdlib"
 	_ "github.com/mattn/go-sqlite3"
+
+	"main/src/gvabe/bo/user"
 )
 
 func newSqlConnectSqlite(driver, url, timezone string, timeoutMs int, poolOptions *prom.SqlPoolOptions) (*prom.SqlConnect, error) {
@@ -171,18 +174,17 @@ func TestAppDaosql_Create(t *testing.T) {
 	}
 	for k, info := range urlMap {
 		var sqlc *prom.SqlConnect
-		var err error
 		switch k {
 		case "sqlite", "sqlite3":
-			sqlc, err = newSqlConnectSqlite(info.driver, info.url, timezoneSql, 10000, nil)
+			sqlc, _ = newSqlConnectSqlite(info.driver, info.url, timezoneSql, 10000, nil)
 		case "mssql":
-			sqlc, err = newSqlConnectMssql(info.driver, info.url, timezoneSql, 10000, nil)
+			sqlc, _ = newSqlConnectMssql(info.driver, info.url, timezoneSql, 10000, nil)
 		case "mysql":
-			sqlc, err = newSqlConnectMysql(info.driver, info.url, timezoneSql, 10000, nil)
+			sqlc, _ = newSqlConnectMysql(info.driver, info.url, timezoneSql, 10000, nil)
 		case "oracle":
-			sqlc, err = newSqlConnectOracle(info.driver, info.url, timezoneSql, 10000, nil)
+			sqlc, _ = newSqlConnectOracle(info.driver, info.url, timezoneSql, 10000, nil)
 		case "pgsql":
-			sqlc, err = newSqlConnectPgsql(info.driver, info.url, timezoneSql, 10000, nil)
+			sqlc, _ = newSqlConnectPgsql(info.driver, info.url, timezoneSql, 10000, nil)
 		default:
 			t.Fatalf("%s failed: unknown database type [%s]", name, k)
 		}
@@ -195,122 +197,201 @@ func TestAppDaosql_Create(t *testing.T) {
 	}
 }
 
-// func TestAppDaoAwsDynamodb_Get(t *testing.T) {
-// 	name := "TestAppDaoAwsDynamodb_Get"
-// 	adc := _createAwsDynamodbConnect(t, name)
-// 	appDao := _initAppDaoDynamodb(t, name, adc)
-// 	appDao.Create(NewApp(1357, "exter", "btnguyen2k", "System application (do not delete)"))
-// 	if app, err := appDao.Get("not_found"); err != nil {
-// 		t.Fatalf("%s failed: %s", name, err)
-// 	} else if app != nil {
-// 		t.Fatalf("%s failed: app %s should not exist", name, "not_found")
-// 	}
-//
-// 	if app, err := appDao.Get("exter"); err != nil {
-// 		t.Fatalf("%s failed: %s", name, err)
-// 	} else if app == nil {
-// 		t.Fatalf("%s failed: nil", name)
-// 	} else {
-// 		if v := app.GetId(); v != "exter" {
-// 			t.Fatalf("%s failed: expected [%#v] but received [%#v]", name, "exter", v)
-// 		}
-// 		if v := app.GetTagVersion(); v != 1357 {
-// 			t.Fatalf("%s failed: expected [%#v] but received [%#v]", name, 1357, v)
-// 		}
-// 		if v := app.GetOwnerId(); v != "btnguyen2k" {
-// 			t.Fatalf("%s failed: expected [%#v] but received [%#v]", name, "btnguyen2k", v)
-// 		}
-// 		if v := app.GetAttrsPublic().Description; v != "System application (do not delete)" {
-// 			t.Fatalf("%s failed: expected [%#v] but received [%#v]", name, "System application (do not delete)", v)
-// 		}
-// 	}
-// }
-//
-// func TestAppDaoAwsDynamodb_Delete(t *testing.T) {
-// 	name := "TestAppDaoAwsDynamodb_Delete"
-// 	adc := _createAwsDynamodbConnect(t, name)
-// 	appDao := _initAppDaoDynamodb(t, name, adc)
-//
-// 	appDao.Create(NewApp(1357, "exter", "btnguyen2k", "System application (do not delete)"))
-// 	app, err := appDao.Get("exter")
-// 	if err != nil {
-// 		t.Fatalf("%s failed: %s", name, err)
-// 	} else if app == nil {
-// 		t.Fatalf("%s failed: nil", name)
-// 	}
-//
-// 	ok, err := appDao.Delete(app)
-// 	if err != nil {
-// 		t.Fatalf("%s failed: %s", name, err)
-// 	} else if !ok {
-// 		t.Fatalf("%s failed: cannot delete app [%s]", name, app.GetId())
-// 	}
-//
-// 	app, err = appDao.Get("exter")
-// 	if app, err := appDao.Get("exter"); err != nil {
-// 		t.Fatalf("%s failed: %s", name, err)
-// 	} else if app != nil {
-// 		t.Fatalf("%s failed: app %s should not exist", name, "exter")
-// 	}
-// }
-//
-// func TestAppDaoAwsDynamodb_Update(t *testing.T) {
-// 	name := "TestAppDaoAwsDynamodb_Update"
-// 	adc := _createAwsDynamodbConnect(t, name)
-// 	appDao := _initAppDaoDynamodb(t, name, adc)
-//
-// 	app := NewApp(1357, "exter", "btnguyen2k", "System application (do not delete)")
-// 	appDao.Create(app)
-//
-// 	app.SetOwnerId("nbthanh")
-// 	app.SetTagVersion(2468)
-// 	app.attrsPublic.Description = "App description"
-// 	ok, err := appDao.Update(app)
-// 	if err != nil || !ok {
-// 		t.Fatalf("%s failed: %#v / %s", name, ok, err)
-// 	}
-//
-// 	if app, err := appDao.Get("exter"); err != nil {
-// 		t.Fatalf("%s failed: %s", name, err)
-// 	} else if app == nil {
-// 		t.Fatalf("%s failed: nil", name)
-// 	} else {
-// 		if v := app.GetId(); v != "exter" {
-// 			t.Fatalf("%s failed: expected [%#v] but received [%#v]", name, "exter", v)
-// 		}
-// 		if v := app.GetTagVersion(); v != 2468 {
-// 			t.Fatalf("%s failed: expected [%#v] but received [%#v]", name, 2468, v)
-// 		}
-// 		if v := app.GetOwnerId(); v != "nbthanh" {
-// 			t.Fatalf("%s failed: expected [%#v] but received [%#v]", name, "nbthanh", v)
-// 		}
-// 		if v := app.GetAttrsPublic().Description; v != "App description" {
-// 			t.Fatalf("%s failed: expected [%#v] but received [%#v]", name, "App description", v)
-// 		}
-// 	}
-// }
-//
-// func TestAppDaoAwsDynamodb_GetUserApps(t *testing.T) {
-// 	name := "TestAppDaoAwsDynamodb_GetUserApps"
-// 	adc := _createAwsDynamodbConnect(t, name)
-// 	appDao := _initAppDaoDynamodb(t, name, adc)
-//
-// 	for i := 0; i < 10; i++ {
-// 		app := NewApp(uint64(i), strconv.Itoa(i), strconv.Itoa(i%3), "App #"+strconv.Itoa(i))
-// 		appDao.Create(app)
-// 	}
-//
-// 	u := user.NewUser(123, "2")
-// 	appList, err := appDao.GetUserApps(u)
-// 	if err != nil {
-// 		t.Fatalf("%s failed: %s", name, err)
-// 	}
-// 	if len(appList) != 3 {
-// 		t.Fatalf("%s failed: expected %#v apps but received %#v", name, 3, len(appList))
-// 	}
-// 	for _, app := range appList {
-// 		if app.GetOwnerId() != "2" {
-// 			t.Fatalf("%s failed: app %#v does not belong to user %#v", name, app.GetId(), "2")
-// 		}
-// 	}
-// }
+func TestAppDaoSql_Get(t *testing.T) {
+	name := "TestAppDaoSql_Get"
+	urlMap := sqlGetUrlFromEnv()
+	if len(urlMap) == 0 {
+		t.Skipf("%s skipped", name)
+	}
+	for k, info := range urlMap {
+		var sqlc *prom.SqlConnect
+		switch k {
+		case "sqlite", "sqlite3":
+			sqlc, _ = newSqlConnectSqlite(info.driver, info.url, timezoneSql, 10000, nil)
+		case "mssql":
+			sqlc, _ = newSqlConnectMssql(info.driver, info.url, timezoneSql, 10000, nil)
+		case "mysql":
+			sqlc, _ = newSqlConnectMysql(info.driver, info.url, timezoneSql, 10000, nil)
+		case "oracle":
+			sqlc, _ = newSqlConnectOracle(info.driver, info.url, timezoneSql, 10000, nil)
+		case "pgsql":
+			sqlc, _ = newSqlConnectPgsql(info.driver, info.url, timezoneSql, 10000, nil)
+		default:
+			t.Fatalf("%s failed: unknown database type [%s]", name, k)
+		}
+		appDao := _initAppDaoSql(t, name, sqlc)
+		appDao.Create(NewApp(1357, "exter", "btnguyen2k", "System application (do not delete)"))
+		if app, err := appDao.Get("not_found"); err != nil {
+			t.Fatalf("%s failed: %s", name, err)
+		} else if app != nil {
+			t.Fatalf("%s failed: app %s should not exist", name, "not_found")
+		}
+
+		if app, err := appDao.Get("exter"); err != nil {
+			t.Fatalf("%s failed: %s", name, err)
+		} else if app == nil {
+			t.Fatalf("%s failed: nil", name)
+		} else {
+			if v := app.GetId(); v != "exter" {
+				t.Fatalf("%s failed: expected [%#v] but received [%#v]", name, "exter", v)
+			}
+			if v := app.GetTagVersion(); v != 1357 {
+				t.Fatalf("%s failed: expected [%#v] but received [%#v]", name, 1357, v)
+			}
+			if v := app.GetOwnerId(); v != "btnguyen2k" {
+				t.Fatalf("%s failed: expected [%#v] but received [%#v]", name, "btnguyen2k", v)
+			}
+			if v := app.GetAttrsPublic().Description; v != "System application (do not delete)" {
+				t.Fatalf("%s failed: expected [%#v] but received [%#v]", name, "System application (do not delete)", v)
+			}
+		}
+	}
+}
+
+func TestAppDaoSql_Delete(t *testing.T) {
+	name := "TestAppDaoSql_Delete"
+	urlMap := sqlGetUrlFromEnv()
+	if len(urlMap) == 0 {
+		t.Skipf("%s skipped", name)
+	}
+	for k, info := range urlMap {
+		var sqlc *prom.SqlConnect
+		switch k {
+		case "sqlite", "sqlite3":
+			sqlc, _ = newSqlConnectSqlite(info.driver, info.url, timezoneSql, 10000, nil)
+		case "mssql":
+			sqlc, _ = newSqlConnectMssql(info.driver, info.url, timezoneSql, 10000, nil)
+		case "mysql":
+			sqlc, _ = newSqlConnectMysql(info.driver, info.url, timezoneSql, 10000, nil)
+		case "oracle":
+			sqlc, _ = newSqlConnectOracle(info.driver, info.url, timezoneSql, 10000, nil)
+		case "pgsql":
+			sqlc, _ = newSqlConnectPgsql(info.driver, info.url, timezoneSql, 10000, nil)
+		default:
+			t.Fatalf("%s failed: unknown database type [%s]", name, k)
+		}
+		appDao := _initAppDaoSql(t, name, sqlc)
+		appDao.Create(NewApp(1357, "exter", "btnguyen2k", "System application (do not delete)"))
+		app, err := appDao.Get("exter")
+		if err != nil {
+			t.Fatalf("%s failed: %s", name, err)
+		} else if app == nil {
+			t.Fatalf("%s failed: nil", name)
+		}
+
+		ok, err := appDao.Delete(app)
+		if err != nil {
+			t.Fatalf("%s failed: %s", name, err)
+		} else if !ok {
+			t.Fatalf("%s failed: cannot delete app [%s]", name, app.GetId())
+		}
+
+		app, err = appDao.Get("exter")
+		if app, err := appDao.Get("exter"); err != nil {
+			t.Fatalf("%s failed: %s", name, err)
+		} else if app != nil {
+			t.Fatalf("%s failed: app %s should not exist", name, "exter")
+		}
+	}
+}
+
+func TestAppDaoSql_Update(t *testing.T) {
+	name := "TestAppDaoSql_Update"
+	urlMap := sqlGetUrlFromEnv()
+	if len(urlMap) == 0 {
+		t.Skipf("%s skipped", name)
+	}
+	for k, info := range urlMap {
+		var sqlc *prom.SqlConnect
+		switch k {
+		case "sqlite", "sqlite3":
+			sqlc, _ = newSqlConnectSqlite(info.driver, info.url, timezoneSql, 10000, nil)
+		case "mssql":
+			sqlc, _ = newSqlConnectMssql(info.driver, info.url, timezoneSql, 10000, nil)
+		case "mysql":
+			sqlc, _ = newSqlConnectMysql(info.driver, info.url, timezoneSql, 10000, nil)
+		case "oracle":
+			sqlc, _ = newSqlConnectOracle(info.driver, info.url, timezoneSql, 10000, nil)
+		case "pgsql":
+			sqlc, _ = newSqlConnectPgsql(info.driver, info.url, timezoneSql, 10000, nil)
+		default:
+			t.Fatalf("%s failed: unknown database type [%s]", name, k)
+		}
+		appDao := _initAppDaoSql(t, name, sqlc)
+
+		app := NewApp(1357, "exter", "btnguyen2k", "System application (do not delete)")
+		appDao.Create(app)
+
+		app.SetOwnerId("nbthanh")
+		app.SetTagVersion(2468)
+		app.attrsPublic.Description = "App description"
+		ok, err := appDao.Update(app)
+		if err != nil || !ok {
+			t.Fatalf("%s failed: %#v / %s", name, ok, err)
+		}
+
+		if app, err := appDao.Get("exter"); err != nil {
+			t.Fatalf("%s failed: %s", name, err)
+		} else if app == nil {
+			t.Fatalf("%s failed: nil", name)
+		} else {
+			if v := app.GetId(); v != "exter" {
+				t.Fatalf("%s failed: expected [%#v] but received [%#v]", name, "exter", v)
+			}
+			if v := app.GetTagVersion(); v != 2468 {
+				t.Fatalf("%s failed: expected [%#v] but received [%#v]", name, 2468, v)
+			}
+			if v := app.GetOwnerId(); v != "nbthanh" {
+				t.Fatalf("%s failed: expected [%#v] but received [%#v]", name, "nbthanh", v)
+			}
+			if v := app.GetAttrsPublic().Description; v != "App description" {
+				t.Fatalf("%s failed: expected [%#v] but received [%#v]", name, "App description", v)
+			}
+		}
+	}
+}
+
+func TestAppDaoSql_GetUserApps(t *testing.T) {
+	name := "TestAppDaoSql_GetUserApps"
+	urlMap := sqlGetUrlFromEnv()
+	if len(urlMap) == 0 {
+		t.Skipf("%s skipped", name)
+	}
+	for k, info := range urlMap {
+		var sqlc *prom.SqlConnect
+		switch k {
+		case "sqlite", "sqlite3":
+			sqlc, _ = newSqlConnectSqlite(info.driver, info.url, timezoneSql, 10000, nil)
+		case "mssql":
+			sqlc, _ = newSqlConnectMssql(info.driver, info.url, timezoneSql, 10000, nil)
+		case "mysql":
+			sqlc, _ = newSqlConnectMysql(info.driver, info.url, timezoneSql, 10000, nil)
+		case "oracle":
+			sqlc, _ = newSqlConnectOracle(info.driver, info.url, timezoneSql, 10000, nil)
+		case "pgsql":
+			sqlc, _ = newSqlConnectPgsql(info.driver, info.url, timezoneSql, 10000, nil)
+		default:
+			t.Fatalf("%s failed: unknown database type [%s]", name, k)
+		}
+		appDao := _initAppDaoSql(t, name, sqlc)
+
+		for i := 0; i < 10; i++ {
+			app := NewApp(uint64(i), strconv.Itoa(i), strconv.Itoa(i%3), "App #"+strconv.Itoa(i))
+			appDao.Create(app)
+		}
+
+		u := user.NewUser(123, "2")
+		appList, err := appDao.GetUserApps(u)
+		if err != nil {
+			t.Fatalf("%s failed: %s", name, err)
+		}
+		if len(appList) != 3 {
+			t.Fatalf("%s failed: expected %#v apps but received %#v", name, 3, len(appList))
+		}
+		for _, app := range appList {
+			if app.GetOwnerId() != "2" {
+				t.Fatalf("%s failed: app %#v does not belong to user %#v", name, app.GetId(), "2")
+			}
+		}
+	}
+}
